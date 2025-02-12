216d1a74caf79f6ff7749a8d5902ebde4d82d5dc
瞑尘 <minchen3943@gmail.com>
2025-02-13 02:47:24 +0800

    feat

--- react/src/components/Navbar.tsx
+++ react/src/components/Navbar.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { Link } from "next-view-transitions";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   const linkList = [
     { name: "首页", link: "/" },
     { name: "博客", link: "/blog" },
@@ -11,12 +14,16 @@ export default function Navbar() {
     <>
       <div className="w-[80vw] lg:w-[25vw] h-[6vh] lg:h-[4.5vh] mx-auto my-3 border-[#5b5b5b35] bg-[#ffffffcc] rounded-full drop-shadow-lg border">
         <div className="flex h-full items-center justify-center">
-          <div className="flex w-full justify-evenly">
+          <div className="flex w-full h-full justify-evenly">
             {linkList.map((item) => (
               <div
                 key={item.name}
-                className="text-lg p-1 rounded-lg font-sans w-16 text-center">
-                <Link href={item.link} className="h-full w-full">
+                className={`text-lg  rounded-lg font-sans w-16 h-full text-center relative ${
+                  pathname === item.link
+                    ? "text-[#9A73B5]"
+                    : "text-[#4A4A4A] hover:text-[#D6C6E1]"
+                }`}>
+                <Link href={item.link} className="h-full w-full block p-1">
                   {item.name}
                 </Link>
               </div>
